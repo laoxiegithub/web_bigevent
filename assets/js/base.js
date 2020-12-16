7c626a8df40b701e34f0c53e44446860009ec26e
laoxie <1078116634@qq.com>
2020-12-16 15:25:01 +0800

    完成了index首页开发

--- assets/js/base.js
+++ assets/js/base.js
@@ -6,6 +6,12 @@ $.ajaxPrefilter( function(options) {
      options.header = {
         Authotization:localStorage.getItem('token') || ''
      }
-    
+    options.complete =function(res){
+            console.log(res);
+            if(res.responseJSON.status==1&& res.responseJSON.message=="身份认证失败！") {
+                localStorage.removeItem('token');
+      location.href = '../../home/login.html'
+            }
+    }
  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,11 @@ function getUserInfo () {
         success:function(res) {
             if(res.status ==1 ) {
                 return layui.layer.msg('获取用户信息失败')
+
             }
          render(res.data)
-        }
+        },
+      
     })
 }
 // 判断用户是否有图像
@@ -30,5 +32,16 @@ if(user.user_pic !==null) {
    
 }
 }
+// 实现用户点击退出按钮弹出询问框 并点击确定能够实现退出功能
+$(".close").on('click',function(){
+    layer.confirm('是否确认退出', {icon: 3, title:'提示'}, function(index){
+    //   点击弹出层退出需要做的事情在这里填写
+    // 1 需要情况token的值
+    //2 需要跳转到login页面
+      localStorage.removeItem('token');
+      location.href = '../../home/login.html'  
+        layer.close(index);
+      });
+})
 })
 
